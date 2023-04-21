7d5dcf49926ea1612491bbbb1fd00d7fff48597f
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-04-21 12:23:50 +0200

    refactory

--- badwords.php
+++ badwords.php
@@ -16,37 +16,27 @@ $length_text_censure = strlen($text_censure);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words</title>
 </head>
+
 <body>
     <h1>Bad Words</h1>
     <h2>Il nostro testo: </h2>
-        <p>
-        <?php echo $text;
-        ?>
-    </p>
+    <p><?php echo $text; ?> </p>
     <p>Parola da censurare: 
-
-        <?php echo $badword;
-        ?>
-    </p>
+        <?php echo $badword; ?></p>
     <p>Lunghezza del nostro testo: 
-    <?php echo $length_text;
-        ?> caratteri.
-    </p>
+        <?php echo $length_text; ?> caratteri.</p>
     <h2>Testo censurato:</h2>
-    <p>
-    <?php echo $text_censure;
-        ?>
-    </p>
+    <p><?php echo $text_censure; ?></p>
     <p>Lunghezza del testo censurato: 
-    <?php echo $length_text_censure;
-        ?> caratteri.
-    </p>
+        <?php echo $length_text_censure; ?> caratteri.</p>
 
 </body>
+
 </html>
